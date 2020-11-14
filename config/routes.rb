6935ab3372fe7f6151f43cb214c54e734d23440f
Simon Gebreselassie <simonabra35@gmail.com>
2020-11-14 18:19:33 -0500

    finish toy app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
-  root 'application#hello'
+  resources :mircoposts
+  resources :users
+
+  root 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
